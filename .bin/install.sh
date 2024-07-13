bc59bfc00d132c33e772ab90eaae8269f52766a0
megutamago <naoya05280708@gmail.com>
2024-07-13 22:08:06 +0900

    vscode警告修正、readme修正

--- .bin/install.sh
+++ .bin/install.sh
@@ -1,5 +1,4 @@
-#!/bin/bash
-set -eux
+#!/bin/bash -eux
 
 helpmsg() {
   command echo "Usage: $0 [--help | -h]" 0>&2
@@ -7,45 +6,48 @@ helpmsg() {
 }
 
 link_to_homedir() {
+  local script_dir
+  local dotdir
+
   command echo "backup old dotfiles..."
-  if [ ! -d "$HOME/.dotbackup" ];then
+  if [ ! -d "$HOME/.dotbackup" ]; then
     command echo "$HOME/.dotbackup not found. Auto Make it"
     command mkdir "$HOME/.dotbackup"
   fi
 
-  local script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
-  local dotdir=$(dirname ${script_dir})
-  if [[ "$HOME" != "$dotdir" ]];then
-    for f in $dotdir/.??*; do
-      [[ `basename $f` == ".git" ]] && continue
-      if [[ -L "$HOME/`basename $f`" ]];then
-        command rm -f "$HOME/`basename $f`"
+  script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
+  dotdir=$(dirname "${script_dir}")
+  if [[ "$HOME" != "$dotdir" ]]; then
+    for f in "$dotdir"/.??*; do
+      [[ $(basename "$f") == ".git" ]] && continue
+      if [[ -L "$HOME/$(basename "$f")" ]]; then
+        command rm -f "$HOME/$(basename "$f")"
       fi
-      if [[ -e "$HOME/`basename $f`" ]];then
-        command mv "$HOME/`basename $f`" "$HOME/.dotbackup"
+      if [[ -e "$HOME/$(basename "$f")" ]]; then
+        command mv "$HOME/$(basename "$f")" "$HOME/.dotbackup"
       fi
-      command ln -snf $f $HOME
+      command ln -snf "$f" "$HOME"
     done
   else
     command echo "same install src dest"
   fi
 }
 
-while [ $# -gt 0 ];do
+while [ $# -gt 0 ]; do
   case ${1} in
-    --debug|-d)
-      set -uex
-      ;;
-    --help|-h)
-      helpmsg
-      exit 1
-      ;;
-    *)
-      ;;
+  --debug | -d)
+    set -uex
+    ;;
+  --help | -h)
+    helpmsg
+    exit 1
+    ;;
+  *) ;;
+
   esac
   shift
 done
 
 link_to_homedir
-git config --global include.path "~/.gitconfig_shared"
+git config --global include.path "$HOME/.gitconfig_shared"
 command echo -e "\e[1;36m Install completed!!!! \e[m"
--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # dotfiles
 
-```
+## Procedure
+
+```bash
 cd $HOME && \
 git clone https://github.com/megutamago/dotfiles.git && \
 cd dotfiles && \
@@ -8,10 +10,8 @@ chmod +x .bin/install.sh && \
 ./.bin/install.sh
 ```
 
-```
+```bash
 git config --global user.email "you@example.com"
 git config --global user.name "Your Name"
 git config --global credential.helper store
 ```
-git pull or push でアクセストークン入力後、自動で記憶。
-
