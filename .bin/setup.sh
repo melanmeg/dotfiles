272798783c62a09ac4e85be60984d802b005a9d9
melanmeg <naoya05280708@gmail.com>
2024-07-14 02:02:28 +0900

    WIP

---
+++ .bin/setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -eu
+
+### Ubuntu22.04 ###
+ubuntu_setup() {
+  sudo apt purge -y needrestart
+  sudo timedatectl set-timezone Asia/Tokyo
+  sudo systemctl restart rsyslog
+  # mirror set riken
+  sudo sed -i.bak -r 's!http://(security|us.archive).ubuntu.com/ubuntu!http://ftp.riken.jp/Linux/ubuntu!' /etc/apt/sources.list
+  sudo apt update -y && apt upgrade -y
+  # set inputrc
+  echo "set bell-style none" | sudo tee -a /etc/inputrc
+}
+
+main() {
+  ubuntu_setup
+  echo -e "\e[1;36m Setup completed!!!! \e[m"
+}
+
+main "$@"
