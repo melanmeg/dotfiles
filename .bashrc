bbad69dc57d8943e99ed3209c80a9ea6b735827e
melanmeg <naoya05280708@gmail.com>
2024-07-15 23:01:33 +0900

    WIP

---
+++ .bashrc
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+[ -z "$PS1" ] && return
+HISTCONTROL=ignoredups:ignorespace
+shopt -s histappend
+HISTSIZE=1000
+HISTFILESIZE=2000
+shopt -s checkwinsize
+
+[ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
+if [ -z "$debian_chroot" ] && [ -r /etc/debian_chroot ]; then
+  debian_chroot=$(cat /etc/debian_chroot)
+fi
+
+case "$TERM" in
+xterm-color) color_prompt=yes ;;
+esac
+
+if [ -n "$force_color_prompt" ]; then
+  if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
+    color_prompt=yes
+  else
+    color_prompt=
+  fi
+fi
+
+if [ "$color_prompt" = yes ]; then
+  PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
+else
+  PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w\$ '
+fi
+unset color_prompt force_color_prompt
+
+case "$TERM" in
+xterm* | rxvt*)
+  PS1="\[\e]0;${debian_chroot:+($debian_chroot)}\u@\h: \w\a\]$PS1"
+  ;;
+*) ;;
+esac
+
+if [ -x /usr/bin/dircolors ]; then
+  # shellcheck disable=SC2015
+  test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+  alias ls='ls --color=auto'
+  alias dir='dir --color=auto'
+  alias vdir='vdir --color=auto'
+  alias grep='grep --color=auto'
+  alias fgrep='fgrep --color=auto'
+  alias egrep='egrep --color=auto'
+fi
+
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+
+if [ -f ~/.bash_aliases ]; then
+  # shellcheck disable=SC1090
+  . ~/.bash_aliases
+fi
+
+# ================================
+#  Added settings
+# ================================
+
+# Load bashrc.d
+for file in ~/.bashrc.d/*; do
+  # shellcheck disable=SC1090
+  source "$file"
+done
---
+++ .bashrc.d/base.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+# ================================
+#  Added settings
+# ================================
+
+# git
+alias gitpush='git add -A && git commit -m "submit" && git push origin main'
+
+# docker
+alias d='docker'
+alias dp='docker ps -a'
+alias drm='docker rm -f $(docker ps -aq)'
+alias drmi='docker rmi -f $(docker images -q)'
+alias deli='sh -xc "docker rm -f $(docker ps -aq); docker volume prune -a -f; docker system prune -a -f"'
+alias dsys='docker system df'
+
+# docker-compose
+alias dc='docker-compose'
+alias dcp='docker-compose ps -a'
+alias wdcp='watch docker-compose ps -a'
+alias dcb='docker-compose build'
+alias dcu='docker-compose up -d'
+alias dcd='docker-compose down'
+alias dceli='docker-compose down --rmi all --volumes --remove-orphans'
+
+# ansible
+alias play='ansible-playbook -i hosts site.yml'
+alias playc='ansible-playbook -i hosts site.yml -C'
+alias pplay='ANSIBLE_FORKS=20 ansible-playbook -i hosts site.yml'
+alias pplayc='ANSIBLE_FORKS=20 ansible-playbook -i hosts site.yml -C'
+
+# terraform
+alias tf='terraform'
+alias tfi='terraform init'
+alias tfs='terraform show .terraform/state.tfstate'
+alias tfl='terraform state list -state=.terraform/state.tfstate'
+alias tfp='terraform plan -state=.terraform/state.tfstate'
+alias tfa='terraform apply --auto-approve -state=.terraform/state.tfstate'
+alias tfd='terraform destroy --auto-approve -state=.terraform/state.tfstate'
+alias tfr='terraform refresh -state=.terraform/state.tfstate'
+
+# clear
+alias clr='rm ~/.ssh/known_hosts'
+alias mclear='echo 3 > /proc/sys/vm/drop_caches'
+
+# ssh
+alias basessh='ssh root@192.168.11.11'
+
+# python
+alias pyformat='pysen run format && pysen run lint'
+
+# iostat
+alias myio="iostat -cdmhNtz 1"
+
+# k6
+alias format='npm run format --prefix ./scenarios/'
+perf() {
+  docker-compose run k6 -c "
+  k6 --url https://localhost \
+    --username admin \
+    --password admin \
+    --out-dir /result \
+    api/*/*/${1}.js
+  "
+}
+perfall() {
+  npm run format --prefix ./scenarios/
+  docker-compose build
+  docker-compose run k6 -c "
+  k6 --url https://localhost \
+    --username admin \
+    --password admin \
+    --out-dir /result \
+    api/*/*/${1}.js
+  "
+}
+
+# grep
+# alias grepclean"grep -v -e '^\s*#' -e '^\s*$'"
+
+mypsqlcolumn() {
+  docker-compose exec database psql -U pguser database -P 'pager=off' -c "select column_name from information_schema.columns where table_schema = 'public' AND table_name = '$1';"
+}
+
+# keygen
+alias keygen='ssh-keygen -t ed25519 -C "" -f ./keygen -N ""'
