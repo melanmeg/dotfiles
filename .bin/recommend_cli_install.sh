2efef628e47709c0505972757532cdd7342b0456
melanmeg <naoya05280780@gmail.com>
2024-07-27 20:58:20 +0900

    hogehoge

--- .bin/recommend_cli_install.sh
+++ .bin/recommend_cli_install.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -eu
 
-# 工事中
-
 # Setup my poetry
 # - thefuck
 # - glances
@@ -14,12 +12,14 @@ poetry install --directory "$HOME/.my/poetry" --no-root
 # - aria2
 sudo apt install -y aria2
 
-# - bandwhich # ネットワーク帯域幅の使用状況をリアルタイムで監視
+# - bandwhich > ネットワーク帯域幅の使用状況をリアルタイムで監視
 PACKAGE_NAME=bandwhich
 aria2c -d /tmp -x 16 -s 16 -k 1M -o $PACKAGE_NAME.tar.gz https://github.com/imsnif/bandwhich/releases/download/v0.22.2/bandwhich-v0.22.2-x86_64-unknown-linux-musl.tar.gz \
   tar -xzf /tmp/$PACKAGE_NAME.tar.gz -C /tmp \
   sudo cp -a /tmp/$PACKAGE_NAME /usr/local/bin \
-  rm -rf /tmp/$PACKAGE_NAME*
+  rm -rf /tmp/$PACKAGE_NAME* \
+  sudo chown "$USER:$USER" /usr/local/bin/$PACKAGE_NAME \
+  sudo chmod +x /usr/local/bin/$PACKAGE_NAME
 
 # - gping
 sudo apt install -y gping
@@ -29,25 +29,31 @@ PACKAGE_NAME=lazydocker
 aria2c -d /tmp -x 16 -s 16 -k 1M -o $PACKAGE_NAME.tar.gz https://github.com/jesseduffield/lazydocker/releases/download/v0.23.3/lazydocker_0.23.3_Linux_x86_64.tar.gz \
   tar -xzf /tmp/$PACKAGE_NAME.tar.gz -C /tmp \
   sudo cp -a /tmp/$PACKAGE_NAME /usr/local/bin \
-  rm -rf /tmp/$PACKAGE_NAME*
+  rm -rf /tmp/$PACKAGE_NAME* \
+  sudo chown "$USER:$USER" /usr/local/bin/$PACKAGE_NAME \
+  sudo chmod +x /usr/local/bin/$PACKAGE_NAME
 
-# - figlet # ASCIIアート
+# - figlet > ASCIIアート
 
-# - lolcat # テキストを虹色に
+# - lolcat > テキストを虹色に
 
-# - actionlint # GitHub Actions の Workflow ファイルの静的検査
+# - actionlint > GitHub Actions の Workflow ファイルの静的検査
 
-# - aicommits # コミットメッセージを自動生成
+# - aicommits > コミットメッセージを自動生成
 
-# - cLive # ターミナル操作を自動化
+# - cLive > ターミナル操作を自動化
 
-# - genact # 忙しいフリをする
+# - genact > 忙しいフリをする
+PACKAGE_NAME=genact
+sudo curl -Lo /usr/local/bin/$PACKAGE_NAME https://github.com/svenstaro/genact/releases/download/v1.4.2/genact-1.4.2-x86_64-unknown-linux-musl \
+  sudo chown "$USER:$USER" /usr/local/bin/$PACKAGE_NAME \
+  sudo chmod +x /usr/local/bin/$PACKAGE_NAME
 
-# - gh # GitHub CLI
+# - gh > GitHub CLI
 
 # - hyperfine -> Installed from mise
 
-# - charming (rust)
+# - charming
 
 # - farm
 
