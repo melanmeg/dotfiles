f58319ffa2b08fd06cf6f057f8a15db6f40302a9
melanmeg <naoya05280780@gmail.com>
2024-07-28 12:16:55 +0900

    un

--- .bin/recommend_cli_install.sh
+++ .bin/recommend_cli_install.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 set -eu
 
+# Generic function to download binary files
+install_package() {
+  PACKAGE_NAME=$1
+  URL=$2
+  aria2c -d /tmp -x 16 -s 16 -k 1M -o "$PACKAGE_NAME.tar.gz" "$URL"
+  tar -xzf "/tmp/$PACKAGE_NAME.tar.gz" -C /tmp
+  sudo cp -a "/tmp/$PACKAGE_NAME" /usr/local/bin
+  rm -rf "/tmp/$PACKAGE_NAME*"
+  sudo chown "$USER:$USER" "/usr/local/bin/$PACKAGE_NAME"
+  sudo chmod +x "/usr/local/bin/$PACKAGE_NAME"
+}
+
 # Setup my poetry
 # - thefuck
 # - glances
@@ -13,37 +25,19 @@ poetry install --directory "$HOME/.my/poetry" --no-root
 sudo apt install -y aria2
 
 # - bandwhich > ネットワーク帯域幅の使用状況をリアルタイムで監視
-PACKAGE_NAME=bandwhich
-aria2c -d /tmp -x 16 -s 16 -k 1M -o $PACKAGE_NAME.tar.gz https://github.com/imsnif/bandwhich/releases/download/v0.22.2/bandwhich-v0.22.2-x86_64-unknown-linux-musl.tar.gz
-tar -xzf /tmp/$PACKAGE_NAME.tar.gz -C /tmp
-sudo cp -a /tmp/$PACKAGE_NAME /usr/local/bin
-rm -rf /tmp/$PACKAGE_NAME*
-sudo chown "$USER:$USER" /usr/local/bin/$PACKAGE_NAME
-sudo chmod +x /usr/local/bin/$PACKAGE_NAME
+install_package bandwhich https://github.com/imsnif/bandwhich/releases/download/v0.22.2/bandwhich-v0.22.2-x86_64-unknown-linux-musl.tar.gz
 
 # - gping
 sudo apt install -y gping
 
 # - lagydocker
-PACKAGE_NAME=lazydocker
-aria2c -d /tmp -x 16 -s 16 -k 1M -o $PACKAGE_NAME.tar.gz https://github.com/jesseduffield/lazydocker/releases/download/v0.23.3/lazydocker_0.23.3_Linux_x86_64.tar.gz
-tar -xzf /tmp/$PACKAGE_NAME.tar.gz -C /tmp
-sudo cp -a /tmp/$PACKAGE_NAME /usr/local/bin
-rm -rf /tmp/$PACKAGE_NAME*
-sudo chown "$USER:$USER" /usr/local/bin/$PACKAGE_NAME
-sudo chmod +x /usr/local/bin/$PACKAGE_NAME
+install_package lazydocker https://github.com/jesseduffield/lazydocker/releases/download/v0.23.3/lazydocker_0.23.3_Linux_x86_64.tar.gz
 
 # - figlet > ASCIIアート
 sudo apt install -y figlet # figlet Hello, World!
 
 # - actionlint > GitHub Actions の Workflow ファイルの静的検査
-PACKAGE_NAME=actionlint
-aria2c -d /tmp -x 16 -s 16 -k 1M -o $PACKAGE_NAME.tar.gz https://github.com/rhysd/actionlint/releases/download/v1.7.1/actionlint_1.7.1_linux_amd64.tar.gz
-tar -xzf /tmp/$PACKAGE_NAME.tar.gz -C /tmp
-sudo cp -a /tmp/$PACKAGE_NAME /usr/local/bin
-rm -rf /tmp/$PACKAGE_NAME*
-sudo chown "$USER:$USER" /usr/local/bin/$PACKAGE_NAME
-sudo chmod +x /usr/local/bin/$PACKAGE_NAME
+install_package actionlint https://github.com/rhysd/actionlint/releases/download/v1.7.1/actionlint_1.7.1_linux_amd64.tar.gz
 
 # - aicommits > コミットメッセージを自動生成
 # ???
@@ -51,6 +45,7 @@ sudo chmod +x /usr/local/bin/$PACKAGE_NAME
 # aicommits config set OPENAI_KEY=<your token>
 
 # - cLive > ターミナル操作を自動化
+install_package clive https://github.com/koki-develop/clive/releases/download/v0.12.9/clive_Linux_x86_64.tar.gz
 
 # - genact > 忙しいフリをする
 PACKAGE_NAME=genact
@@ -59,13 +54,23 @@ sudo chown "$USER:$USER" /usr/local/bin/$PACKAGE_NAME
 sudo chmod +x /usr/local/bin/$PACKAGE_NAME
 
 # - gh > GitHub CLI
+(type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) &&
+  sudo mkdir -p -m 755 /etc/apt/keyrings &&
+  wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg >/dev/null &&
+  sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg &&
+  echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list >/dev/null &&
+  sudo apt update &&
+  sudo apt install gh -y
 
-# - hyperfine -> Installed from mise
+# - pocketbase
+install_package clive https://github.com/pocketbase/pocketbase/releases/download/v0.22.18/pocketbase_0.22.18_linux_amd64.zip
 
 # - charming
+# cargo add charming
 
 # - farm
 
-# - pocketbase
-
 # - go-zero
+# go get -u github.com/zeromicro/go-zero
+
+# - hyperfine -> Installed from mise
