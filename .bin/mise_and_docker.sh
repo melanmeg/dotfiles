beb4d55575ae453dd7353615b771b4e83f7764dd
melanmeg <naoya05280780@gmail.com>
2024-07-29 22:58:50 +0900

    fuga

--- .bin/mise_and_docker.sh
+++ .bin/mise_and_docker.sh
@@ -2,10 +2,13 @@
 set -eu
 
 # Install docker
-curl -fsSL https://get.docker.com | sh
+sudo curl -fsSL https://get.docker.com | sh
 
 # Install dokcer compose
-curl -L https://github.com/docker/compose/releases/download/v2.29.0/docker-compose-$(uname -s)-$(uname -m) >/usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/v2.29.0/docker-compose-$(uname -s)-$(uname -m) >/usr/local/bin/docker-compose
+
+sudo curl -L https://github.com/docker/compose/releases/download/v2.29.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 # Add docker Group
 sudo usermod -aG docker "$USER"
