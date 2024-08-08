8fc6900d0aed1862f8ef756ceaa1c3665a3a24b7
melanmeg <naoya05280780@gmail.com>
2024-08-08 11:47:34 +0900

    🥖バゲット

--- .bin/install-clis.sh
+++ .bin/install-clis.sh
@@ -27,45 +27,45 @@ poetry install --directory "$HOME/.my/poetry" --no-root
 # rm -rf "$(poetry env list --full-path --directory "$HOME/.my/poetry" | grep -o '/.*')"
 
 ### Setup my nodejs
-# - aicommits > コミットメッセージを自動生成
+# - aicommits
 # .envにOPENAI_KEY=<your token>を設定
 npm i --prefix "$HOME/.my/nodejs"
 
 ### Setup apt install
 # - aria2
 # - gping
-# - figlet > ASCIIアート (e.g., figlet Hello, World!)
+# - figlet
 sudo apt install -y aria2 gping figlet
 
 ### Generic function to download binary files (tar)
 install_tar_package() {
   PACKAGE_NAME=$1
   URL=$2
-  aria2c -d /tmp -x 16 -s 16 -k 1M -o "$PACKAGE_NAME".tar.gz "$URL"
-  tar -xzf /tmp/"$PACKAGE_NAME".tar.gz -C /tmp
-  sudo cp -a /tmp/"$PACKAGE_NAME" /usr/local/bin
-  rm -rf /tmp/"$PACKAGE_NAME"*
-  sudo chown "$USER":"$USER" /usr/local/bin/"$PACKAGE_NAME"
-  sudo chmod +x /usr/local/bin/"$PACKAGE_NAME"
+  aria2c -d /tmp -x 16 -s 16 -k 1M -o "$PACKAGE_NAME.tar.gz" "$URL"
+  tar -xzf "/tmp/$PACKAGE_NAME.tar.gz" -C /tmp
+  sudo cp -a "/tmp/$PACKAGE_NAME" "$HOME/.local/bin"
+  rm -rf "/tmp/$PACKAGE_NAME"*
+  sudo chown "$USER:$USER" "$HOME/.local/bin/$PACKAGE_NAME"
+  sudo chmod +x "$HOME/.local/bin/$PACKAGE_NAME"
 }
 
 ### Generic function to download binary files (zip)
 install_zip_package() {
   PACKAGE_NAME=$1
   URL=$2
-  aria2c -d /tmp -x 16 -s 16 -k 1M -o "$PACKAGE_NAME".zip "$URL"
-  unzip /tmp/"$PACKAGE_NAME".zip -d /tmp
-  sudo cp -a /tmp/"$PACKAGE_NAME" /usr/local/bin
-  rm -rf /tmp/"$PACKAGE_NAME"*
-  sudo chown "$USER":"$USER" /usr/local/bin/"$PACKAGE_NAME"
-  sudo chmod +x /usr/local/bin/"$PACKAGE_NAME"
+  aria2c -d /tmp -x 16 -s 16 -k 1M -o "$PACKAGE_NAME.zip" "$URL"
+  unzip "/tmp/$PACKAGE_NAME.zip" -d /tmp
+  sudo cp -a "/tmp/$PACKAGE_NAME" $HOME/.local/bin
+  rm -rf "/tmp/$PACKAGE_NAME"*
+  sudo chown "$USER:$USER" "$HOME/.local/bin/$PACKAGE_NAME"
+  sudo chmod +x "$HOME/.local/bin/$PACKAGE_NAME"
 }
 
 ### Setup install_package
-# - bandwhich > ネットワーク帯域幅の使用状況をリアルタイムで監視
+# - bandwhich
 # - lagydocker
-# - actionlint > GitHub Actions の Workflow ファイルの静的検査
-# - cLive > ターミナル操作を自動化
+# - actionlint
+# - cLive
 # - pocketbase
 install_tar_package bandwhich https://github.com/imsnif/bandwhich/releases/download/v0.22.2/bandwhich-v0.22.2-x86_64-unknown-linux-musl.tar.gz
 install_tar_package lazydocker https://github.com/jesseduffield/lazydocker/releases/download/v0.23.3/lazydocker_0.23.3_Linux_x86_64.tar.gz
@@ -84,12 +84,12 @@ install_zip_package procs https://github.com/dalance/procs/releases/download/v0.
 # - hyperfine -> Installed from mise
 
 ### Other
-# - genact > 忙しいフリをする
+# - genact
 PACKAGE_NAME=genact
-sudo curl -Lo /usr/local/bin/$PACKAGE_NAME https://github.com/svenstaro/genact/releases/download/v1.4.2/genact-1.4.2-x86_64-unknown-linux-musl
-sudo chown "$USER:$USER" /usr/local/bin/$PACKAGE_NAME
-sudo chmod +x /usr/local/bin/$PACKAGE_NAME
-# - gh > GitHub CLI
+sudo curl -Lo "$HOME/.local/bin/$PACKAGE_NAME" https://github.com/svenstaro/genact/releases/download/v1.4.2/genact-1.4.2-x86_64-unknown-linux-musl
+sudo chown "$USER:$USER" "$HOME/.local/bin/$PACKAGE_NAME"
+sudo chmod +x "$HOME/.local/bin/$PACKAGE_NAME"
+# - gh
 (type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) &&
   sudo mkdir -p -m 755 /etc/apt/keyrings &&
   wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg >/dev/null &&
