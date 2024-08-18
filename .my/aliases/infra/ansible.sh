f1164de41d2f04310dd78479b2fa2a6a0c9930d7
melanmeg <naoya05280708@gmail.com>
2024-08-19 00:30:34 +0900

    ansible

--- .my/aliases/infra/ansible.sh
+++ .my/aliases/infra/ansible.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 # ansible
-alias play="ansible-playbook -f 30 --key-file ~/.ssh/main -i hosts site.yml -e 'ansible_python_interpreter=/usr/bin/python3'"
-alias playc="ansible-playbook -f 30 --key-file ~/.ssh/main -i hosts site.yml -C -e 'ansible_python_interpreter=/usr/bin/python3'"
+alias play="ansible-playbook --key-file ~/.ssh/main -i hosts site.yml"
+alias playc="ansible-playbook --key-file ~/.ssh/main -i hosts site.yml -C"
+alias play-env="ansible-playbook -f 30 --key-file ~/.ssh/main -i hosts site.yml -e 'ansible_python_interpreter=/usr/bin/python3'"
+alias playc-env="ansible-playbook -f 30 --key-file ~/.ssh/main -i hosts site.yml -C -e 'ansible_python_interpreter=/usr/bin/python3'"
+
