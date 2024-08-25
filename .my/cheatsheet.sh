9337853214fd75f7a51f20941536c4e3ea0eba48
melanmeg <naoya05280708@gmail.com>
2024-08-26 00:18:40 +0900

    memory?

--- .my/cheatsheet.sh
+++ .my/cheatsheet.sh
@@ -51,6 +51,31 @@ mytcpdump      : alias "sudo tcpdump -ntq"
                : (e.g., tcpdump -A -t -n -i eth1 port 80 and host 192.168.11.11)
 # Survey
 lsof           : (e.g., lsof -i :10250)
+# System Cache Commands
+  mclear         : alias "sync; echo 3 | sudo tee /proc/sys/vm/drop_caches"
+    # 1: ページキャッシュ 解放
+    # 2: dentry、inode 解放
+    # 3: ページキャッシュ、dentry、inode 解放
+    # この操作は非破壊的な操作で、ダーティな (dirty) オブジェクトは解放されないので、この操作を行う際は最初に sync を実行しておくべきである。
+    # ref: https://pooh.gr.jp/?p=5477
+  sudo slabtop -o # キャッシュの状態を確認
+  slabtop --once --sort=c | head -n 20
+  myslabtop      : alias "sudo cat /proc/slabinfo | grep "dentry\|obj" | awk '{ for (i=1; i<=NF; i++) { a[NR,i] = $i } } NF>p { p = NF } END { for(j=1; j<=p; j++) { str=a[1,j]; for(i=2; i<=NR; i++){ str=str"\t"a[i,j-1]; } print str } }' | grep '<'"
+  sar -v 0       : dentunusd が negative dentry の数
+  sudo cat /proc/sys/fs/dentry-state # デンタリキャッシュの状態を確認
+  # デンタリキャッシュの状態
+  # 1. nr_dentry: dentryの数
+  # 2. nr_unused: 未使用のdentryの数
+  # 3. age_limit: メモリが不足している場合に次に dcache entry を再要求できるようになるまでの残り時間 (秒数)
+  # 4. want_pages: dentryのために必要なページ数
+  # 5. dummy
+  # 6. dummy
+  # slabキャッシュとは ref: https://qiita.com/bezeklik/items/7e1ac9e5da39261be7bd
+  mymeminfo       : alias "awk '/kB/ {printf "%-20s %10.2f GB\n", $1, $2 / 1024 / 1024}' /proc/meminfo | grep -v 0.00"
+  awk '
+  /^Cached:|^Active\(file\):|^Inactive\(file\):|^KReclaimable:|^Slab:|^SReclaimable:/ {
+      printf "%-20s %10.2f GB\n", $1, $2 / 1024 / 1024
+  }' /proc/meminfo
 
 #################################
 #   Develop                     #
