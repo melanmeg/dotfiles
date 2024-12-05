870f37feed74d22c884124a0a59c75796407fd4c
melanmeg <naoya05280708@gmail.com>
2024-12-06 01:32:48 +0900

    fix ailias

--- .my/aliases/infra/ansible.sh
+++ .my/aliases/infra/ansible.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # ansible
-alias play="ansible-playbook --key-file ~/.ssh/main -i hosts site.yml"
-alias playc="ansible-playbook --key-file ~/.ssh/main -i hosts site.yml -C"
-alias play-env="ansible-playbook -f 30 --key-file ~/.ssh/main -i hosts site.yml -e 'ansible_python_interpreter=/usr/bin/python3'"
-alias playc-env="ansible-playbook -f 30 --key-file ~/.ssh/main -i hosts site.yml -C -e 'ansible_python_interpreter=/usr/bin/python3'"
+alias play="ansible-playbook --key-file ~/.ssh/main -i hosts playbook.yml"
+alias playc="ansible-playbook --key-file ~/.ssh/main -i hosts playbook.yml -C"
+alias play-env="ansible-playbook -f 30 --key-file ~/.ssh/main -i hosts playbook.yml -e 'ansible_python_interpreter=/usr/bin/python3'"
+alias playc-env="ansible-playbook -f 30 --key-file ~/.ssh/main -i hosts playbook.yml -C -e 'ansible_python_interpreter=/usr/bin/python3'"
 
 # with inventory
-alias iplay="ansible-playbook --key-file ~/.ssh/main -i inventory.yml site.yml"
-alias iplayc="ansible-playbook --key-file ~/.ssh/main -i inventory.yml site.yml -C"
+alias iplay="ansible-playbook --key-file ~/.ssh/main -i inventory.yml playbook.yml"
+alias iplayc="ansible-playbook --key-file ~/.ssh/main -i inventory.yml playbook.yml -C"
--- .my/aliases/infra/terraform.sh
+++ .my/aliases/infra/terraform.sh
@@ -3,16 +3,16 @@
 # terraform
 alias tf='terraform'
 alias tfi='terraform init'
-alias tfs='terraform show .terraform/state.tfstate'
-alias tfl='terraform state list -state=.terraform/state.tfstate'
-alias tfp='terraform plan -state=.terraform/state.tfstate'
-alias tfa='terraform apply --auto-approve -state=.terraform/state.tfstate'
-alias tfd='terraform destroy --auto-approve -state=.terraform/state.tfstate'
-alias tfr='terraform refresh -state=.terraform/state.tfstate'
+alias tfs='terraform show '  # .terraform/state.tfstate
+alias tfl='terraform state list'  # -state=.terraform/state.tfstate
+alias tfp='terraform plan'  # -state=.terraform/state.tfstate
+alias tfa='terraform apply --auto-approve'  # -state=.terraform/state.tfstate
+alias tfd='terraform destroy --auto-approve'  # -state=.terraform/state.tfstate
+alias tfr='terraform refresh'  # .terraform/state.tfstate
 
 # gcloud
 function gcloudlogin() {
-  gcloud auth login --update-adc
+  # gcloud auth login --update-adc
   gcloud auth application-default login
 }
 
