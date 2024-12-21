e8c00b31d58bca40e38e84f4989a0a9895136a6c
melanmeg <naoya05280708@gmail.com>
2024-12-21 23:21:11 +0900

    fix

--- .my/switch_cloud/gcp.sh
+++ .my/switch_cloud/gcp.sh
@@ -10,6 +10,10 @@ CREDENTIALS_FILE="$HOME/.config/gcloud/application_default_credentials.json"
 CREDENTIALS_DIR="$HOME/.config/gcloud/tmp_credentials"
 TMP_CREDENTIALS_FILE="$CREDENTIALS_DIR/$PROFILE_NAME.json"
 
+CONFIG_LIST=$(gcloud config configurations list)
+TARGET_PROFILE=$(echo "$CONFIG_LIST" | awk '{print $1}' | grep "$PROFILE_NAME")
+ACTIVE_PROFILE=$(echo "$CONFIG_LIST" | grep 'True' | awk '{print $1}')
+
 # gcloud auth login
 function gcloud_login_check() {
   account_list=$(gcloud auth list --format="value(account, status)")
@@ -24,17 +28,22 @@ function gcloud_login_check() {
   fi
 }
 
+function gcloud_application_login() {
+  printf "\n"
+  echo "Please application login to gcloud."
+  gcloud auth application-default login
+  # gcloud auth application-default revoke --quiet
+  printf "\n"
+}
+
 # gcloud auth application-default login
 function gcloud_application_login_check() {
   if ! output=$(timeout 3 gcloud auth application-default print-access-token 2>&1); then
     echo "$output" 2>&1
+
     echo " Application login Failed."
-
-    rm -f "$CREDENTIALS_FILE" "$TMP_CREDENTIALS_FILE" # remove symbolic link and file
-
-    echo "Please application login to gcloud."
-    gcloud auth application-default login
-    # gcloud auth application-default revoke --quiet
+    rm -f "$TMP_CREDENTIALS_FILE" # remove credentials file
+    gcloud_application_login
 
     echo " Update credentials file."
     gcloud_credentials_set
@@ -43,8 +52,25 @@ function gcloud_application_login_check() {
   fi
 }
 
+function gcloud_credentials_set() {
+  echo " Set credentials file."
+  cp -a "$CREDENTIALS_FILE" "$TMP_CREDENTIALS_FILE"
+}
+
+function gcloud_credentials_set_check() {
+  if [ -e "$TMP_CREDENTIALS_FILE" ]; then
+    echo " File $TMP_CREDENTIALS_FILE exists."
+  else
+    echo " File $TMP_CREDENTIALS_FILE does not exist."
+    gcloud_config_set
+    gcloud_application_login
+    gcloud_credentials_set
+  fi
+}
+
 # gcloud config set
 function gcloud_config_set() {
+  echo " Set gcloud config."
   gcloud config configurations create "$PROFILE_NAME" > /dev/null 2>&1
   gcloud config set account "$ACCOUNT_NAME" > /dev/null 2>&1
   gcloud config set project "$PROJECT_ID" > /dev/null 2>&1
@@ -54,69 +80,50 @@ function gcloud_config_set() {
 }
 
 function gcloud_config_set_check() {
-  CONFIG_LIST=$(gcloud config configurations list)
-  TARGET_PROFILE=$(echo "$CONFIG_LIST" | awk '{print $1}' | grep "$PROFILE_NAME")
-  ACTIVE_PROFILE=$(echo "$CONFIG_LIST" | grep 'True' | awk '{print $1}')
-
   if [[ -z "$TARGET_PROFILE" ]]; then
     echo " Profile $PROFILE_NAME not found."
-    rm -f "$CREDENTIALS_FILE" # remove symbolic link
     gcloud_config_set
-    echo "Please application login to gcloud."
-    gcloud auth application-default login --quiet
-    gcloud config set account "$ACCOUNT_NAME" > /dev/null 2>&1
-    gcloud config set project "$PROJECT_ID" > /dev/null 2>&1
-
-    return
+    gcloud_application_login_check
   else
     echo " Profile $PROFILE_NAME found."
   fi
+}
 
+function gcloud_config_active_check() {
   if [[ "$ACTIVE_PROFILE" != "$PROFILE_NAME" ]]; then
     echo " Profile $PROFILE_NAME is not active."
-    gcloud_credentials_set_check
+    gcloud config configurations activate "$PROFILE_NAME" > /dev/null 2>&1
+    gcloud_config_set
   else
     echo " Profile $PROFILE_NAME is active."
   fi
 }
 
-function gcloud_credentials_set() {
-  cp -a "$CREDENTIALS_FILE" "$TMP_CREDENTIALS_FILE"
-  ln -sf "$TMP_CREDENTIALS_FILE" "$CREDENTIALS_FILE"
-}
+# check
+function check_wrap() {
+  echo "Check configure gcloud."
+  gcloud_login_check
 
-function gcloud_credentials_set_check() {
   if [ ! -d "$CREDENTIALS_DIR" ]; then
     mkdir -p "$CREDENTIALS_DIR"
   fi
 
   rm -f "$CREDENTIALS_FILE" # remove symbolic link
 
-  if [ -e "$TMP_CREDENTIALS_FILE" ]; then
-    echo " File $TMP_CREDENTIALS_FILE exists."
-    ln -s "$TMP_CREDENTIALS_FILE" "$CREDENTIALS_FILE"
-    gcloud config configurations activate "$PROFILE_NAME" > /dev/null 2>&1
-  else
-    echo " File $TMP_CREDENTIALS_FILE does not exist."
-    gcloud config configurations activate "$PROFILE_NAME" > /dev/null 2>&1
-    gcloud config set account "$ACCOUNT_NAME" > /dev/null 2>&1
-    gcloud config set project "$PROJECT_ID" > /dev/null 2>&1
+  gcloud_credentials_set_check
+  gcloud_config_set_check
+  gcloud_config_active_check
 
-    echo "Please application login to gcloud."
-    gcloud auth application-default login
+  ln -sf "$TMP_CREDENTIALS_FILE" "$CREDENTIALS_FILE"
 
-    gcloud_credentials_set
-  fi
+  gcloud_application_login_check
 }
 
 # main
 function main() {
   echo "Enter Project: $PROJECT_ID"
   printf "\n"
-  echo "Check configure gcloud."
-  gcloud_login_check
-  gcloud_config_set_check
-  gcloud_application_login_check
+  check_wrap
   printf "\n"
   gcloud config configurations list
   printf "\n"
