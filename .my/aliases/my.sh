d3b0752f1de4d937edfe81fa62528b84fb967bca
melanmeg <naoya05280708@gmail.com>
2024-12-30 09:59:28 +0900

    fix

--- .my/aliases/my.sh
+++ .my/aliases/my.sh
@@ -3,6 +3,7 @@
 # git
 # shellcheck disable=SC2139
 alias branchclean="git fetch --prune && git branch -v | grep \\[gone] | awk '{print $1}' | xargs git branch -d"
+alias gitpush="git add -A && git commit -m 'fix' && git push"
 
 # clear
 alias clr='rm -f ~/.ssh/known_hosts*'
